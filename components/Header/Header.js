b2507fc4c6a45b3d53410035a3d848a84b1acc3a
Albert Johanson <aj.dev.gt@gmail.com>
2022-03-25 10:42:53 -0600

    [CODE]ordering the code

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,21 +1,35 @@
-import React from 'react'
+/*
+  This example requires Tailwind CSS v2.0+ 
+  
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/forms'),
+    ],
+  }
+  ```
+*/
 import { Fragment } from 'react'
 import { Popover, Transition } from '@headlessui/react'
 import { MenuIcon, XIcon } from '@heroicons/react/outline'
 
 
 const navigation = [
-    { name: 'Product', href: '#' },
-    { name: 'Features', href: '#' },
-    { name: 'Marketplace', href: '#' },
-    { name: 'Company', href: '#' },
-  ]
-  
+  { name: 'Product', href: '#' },
+  { name: 'Features', href: '#' },
+  { name: 'Marketplace', href: '#' },
+  { name: 'Company', href: '#' },
+]
 
-const Header = () => {
-
-    return (
-        <Popover as="header" className="relative">
+export default function Header() {
+  return (
+    <div>
+       <Popover as="header" className="relative">
         <div className="bg-gray-900 pt-6">
           <nav
             className="relative max-w-7xl mx-auto flex items-center justify-between px-4 sm:px-6"
@@ -119,7 +133,6 @@ const Header = () => {
           </Popover.Panel>
         </Transition>
       </Popover>
-    )
-}
-
-export default Header;
+    </div>
+  )
+} 
