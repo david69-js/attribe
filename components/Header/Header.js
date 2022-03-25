00dccb3e9c8c90db5e24c560ac4549f4139d7382
Albert Johanson <aj.dev.gt@gmail.com>
2022-03-25 15:28:48 -0600

    [DELETE] Hamburger menu

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -37,12 +37,12 @@ export default function Header() {
                     alt=""
                   />
                 </a>
-                <div className="-mr-2 flex items-center md:hidden">
+                {/* <div className="-mr-2 flex items-center md:hidden">
                   <Popover.Button className="bg-gray-900 rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-800 focus:outline-none focus:ring-2 focus-ring-inset focus:ring-white">
                     <span className="sr-only">Open main menu</span>
                     <MenuIcon className="h-6 w-6" aria-hidden="true" />
                   </Popover.Button>
-                </div>
+                </div> */}
               </div>
             </div>
           </nav>
